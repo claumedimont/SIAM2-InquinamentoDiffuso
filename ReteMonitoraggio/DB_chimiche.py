f73ed701d12853ecb478d7ed2c4c1d48ae9fda79
Claudia Medina <118851256+claumedimont@users.noreply.github.com>
2024-11-06 15:50:35 +0100

    fixed issues with merging

--- ReteMonitoraggio/DB_chimiche.py
+++ ReteMonitoraggio/DB_chimiche.py
@@ -9,10 +9,16 @@ import os
 
 # Load the Excel files
 in_dir = 'C:/Users/user/OneDrive - Politecnico di Milano/SF2-Inquinamento_diffuso/Elaborazioni/E_AnalisiChimiche/PerConfronto' 
-#chimiche_df = pd.read_excel(os.path.join(in_dir,'idrochimica_tutti_step6_SL_31102024.xlsx'), sheet_name='MEDIA_2015-2018')
-chimiche_df = pd.read_excel(os.path.join(in_dir,'idrochimica_tutti_step6_SL_31102024.xlsx'), sheet_name='MEDIA_2019-2023')
+chimiche_df = pd.read_excel(os.path.join(in_dir,'idrochimica_tutti_step6_SL_31102024.xlsx'), sheet_name='idrochimica_tutt_step6')
+#chimiche_df = pd.read_csv(os.path.join(in_dir,'mediane.csv'))
+anagrafica_df = pd.read_excel(os.path.join(in_dir, 'Anagarafiche aggregate_tutti_SL_061124.xlsx'))
 
-anagrafica_df = pd.read_excel(os.path.join(in_dir, 'AnagaraficheAggregate_tutti_SL_291024.xlsx'))
+# %%
+#fixing possible sources of error during merge
+chimiche_df['ID_PUNTO'] = chimiche_df['ID_PUNTO'].astype(str) #setting as string
+anagrafica_df['ID_PUNTO'] = anagrafica_df['ID_PUNTO'].astype(str)
+chimiche_df['ID_PUNTO'] = chimiche_df['ID_PUNTO'].str.strip() #deleting white spaces
+anagrafica_df['ID_PUNTO'] = anagrafica_df['ID_PUNTO'].str.strip()
 
 # %%
 # Merge the DataFrames on the 'ID' column
@@ -24,5 +30,5 @@ print(merged_df.head())
 
 # %%
 # Save the merged DataFrame to a new Excel file (optional)
-merged_df.to_excel(os.path.join(in_dir,'Merged_2019-2023.xlsx'), index=False)
+merged_df.to_excel(os.path.join(in_dir,'Merged_complete_anagrafica.xlsx'), index=False)
 # %%
