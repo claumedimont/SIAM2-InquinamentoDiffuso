0bf635477b331a66af8ccd2fa41ab091968437c4
Claudia Medina <118851256+claumedimont@users.noreply.github.com>
2025-03-05 17:49:49 +0100

    mediane 5 anni

--- ReteMonitoraggio/Mediane_idrochim.py
+++ ReteMonitoraggio/Mediane_idrochim.py
@@ -6,40 +6,18 @@ Input files:
 Author: Pietro Mazzon
 
 '''
-
+# %%
 # import libraries
 import os
-
 import pandas as pd
-print('Pandas version: '+pd.__version__) #check version
-
 import numpy as np
-print('Numpy version: '+np.__version__) #check version
 
 # set WD
-# cwd = os.getcwd()
-# print(cwd)
 cwd = "c:/Users/user/OneDrive - Politecnico di Milano/SF2-Inquinamento_diffuso/Elaborazioni/E_AnalisiChimiche/" #my directory
+df = pd.read_excel(os.path.join(cwd,'idrochimica_tutti_step6_SL_31102024.xlsx'), sheet_name="idrochimica_tutt_step6")
 
-# set input files dir
-filepath = os.path.join(cwd,'PerConfronto')
-print('La cartella dei file di input è: ',filepath)
-
-# Read the DF
-file = 'idrochimica_tutti_step6_SL_31102024'
-extension = '.xlsx'
-path = os.path.join(filepath,file + extension)
-df = pd.read_excel(path)
-
-# Writes DF as csv
-writepath = os.path.join(filepath,file + '.csv')
-df.to_csv(writepath, index=False)
-
-df.dtypes # variable types
+# %%
 # drop unwanted cols
-
-#df2=df.copy() # for testing
-
 df.drop(
     ['COMUNE', 'Descrizione Punto', 'PUNTO_PRELIEVO', 'Tipo di campione', 'Tipologia di analisi', 
      'Nota Prelievo', 'Nota Prelevatore', 'VALORE_ORIGINE', 'UM', 'FONTE'],
@@ -47,30 +25,18 @@ df.drop(
     inplace=True
 )
 
-
-print(df.columns)
-
-# check for NaN values
-
-# Checking for missing values using isnull()
-df.isnull() # Creates a set of boolean values
-df.isnull().sum() # make the sum to count the missing values -> better, summarize
-
+# %%
 # groupby ID_PUNTO and then by YEAR
-#df2=df.copy() # for testing
 # create a column "YEAR"
 df['ANNO']=df['DATA'].dt.year
-#df.describe()
-#df.head()
-#df.tail()
+df_filtered = df[df['ANNO'].between(2019, 2023)]
 
 # groupby and median values
-result_grouped = df.groupby(['PARAMETRO','ID_PUNTO','ANNO'])['VALORE_MODIFICATO'].median() #select by PARAMETER, point and by year, then calculates the median of the column valore modificato
-
+result_grouped = df_filtered.groupby(['PARAMETRO','ID_PUNTO'])['VALORE_MODIFICATO'].median() #5-year median by PARAMETER & POINT
 result_reset_index = result_grouped.reset_index() # make the series "result" a DF and reset of indexes
-result_reset_index.rename(columns={'VALORE_MODIFICATO': 'MEDIANA_ANNO'}, inplace=True)
-print(result_reset_index.head(10))
+result_reset_index.rename(columns={'VALORE_MODIFICATO': 'MEDIANA_5ANNI'}, inplace=True)
+result_reset_index['MEDIANA_5ANNI'] = result_reset_index['MEDIANA_5ANNI'].round(3)
 
-#writepath2 = os.path.join(filepath,'mediane.csv')
-result_reset_index.to_excel(os.path.join(filepath, "mediane_annuale_anagrafica_all.xlsx")) # save to excel, in the same filepath as the input file
-#print(result_reset_index)
+# %%
+result_reset_index.to_csv(os.path.join(cwd, "mediane_5_anagrafica_all.csv")) 
+# %%
