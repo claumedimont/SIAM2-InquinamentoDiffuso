9b1e81e7392e6aa9a49adee99ac513f88a12cbd8
Claudia Medina <118851256+claumedimont@users.noreply.github.com>
2025-03-12 16:13:14 +0100

    gis prep

--- Regione/GIS_prep.py
+++ Regione/GIS_prep.py
@@ -9,7 +9,7 @@ import os
 # Load the Excel files
 in_dir = 'C:/Users/user/OneDrive - Politecnico di Milano/04_REGIONE/SIAM2-AggiornamentoSitiMisure/Elaborazioni/' 
 tutti_df = pd.read_excel(os.path.join(in_dir, 'SIAM2_EstrazioneAGISCO.xlsx'), sheet_name="Tutti_comuni_SIAM2")
-siti_df = pd.read_excel(os.path.join(in_dir, 'SIAM2_Siti_Agg.xlsx'), sheet_name="SITI_GIS")
+siti_df = pd.read_excel(os.path.join(in_dir, 'SIAM2_Siti_Aggiornati.xlsx'), sheet_name="SITI_GIS")
 
 # %%
 df_coords = siti_df.merge(tutti_df[['COD_SITO', 'ANA_CX', 'ANA_CY']], on='COD_SITO', how='left')
