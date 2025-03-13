2e5a5e3f01389d3339cf14365e15e21d960ee025
Claudia Medina <118851256+claumedimont@users.noreply.github.com>
2025-03-13 13:24:29 +0100

    aggregated complessivo

--- Regione/Aggiornamento.py
+++ Regione/Aggiornamento.py
@@ -18,32 +18,38 @@ import os
 
 # Load the Excel files
 in_dir = 'C:/Users/user/OneDrive - Politecnico di Milano/04_REGIONE/SIAM2-AggiornamentoSitiMisure/Elaborazioni/' 
-sel_df = pd.read_excel(os.path.join(in_dir, 'SIAM2_ComplessivoNuovo.xlsx'))
-agg_df = pd.read_excel(os.path.join(in_dir, 'SIAM2_Siti_Aggiornati.xlsx'), sheet_name="SITI_PRIORITARI")
+sel_df = pd.read_excel(os.path.join(in_dir, 'DaCondividire/SIAM2_Complessivo.xlsx'), sheet_name="TuttiRecords")
+priori_df = pd.read_excel(os.path.join(in_dir, 'DaCondividire/SIAM2_Siti_Aggiornati.xlsx'), sheet_name="SITI_PRIORITARI")
+proced_df = pd.read_excel(os.path.join(in_dir, 'DaCondividire/SIAM2_Siti_Aggiornati.xlsx'), sheet_name="SITI_PROCEDIMENTO")
 
 
 # %%
-#merge points with info about the applied remediation tecnology
-agg_merge = pd.merge(agg_df, sel_df, on='COD_SITO', how='left')
+#merge info no agregation
+cods_priori = priori_df[['COD_SITO']]
+cods_proced = proced_df[['COD_SITO']]
+agg_merge = pd.merge(cods_priori, sel_df, on='COD_SITO', how='left')
+agg_merge["DGR_SITO"] = 'Prioritario'
+agg_merge2 = pd.merge(cods_proced, sel_df, on='COD_SITO', how='left')
+agg_merge2["DGR_SITO"] = 'Procedimento'
+siti_siam = pd.concat([agg_merge, agg_merge2], ignore_index = True)
 #agg_merge = agg_merge.drop(['Provincia', 'Comune'], axis=1)
 
 # %%
-#agg_merge.to_excel(os.path.join(in_dir,"priori_merge.xlsx"))
+siti_siam.to_excel(os.path.join(in_dir,"solo_siti_comp.xlsx"))
+
 # %%
 # ultimate aggregation
 # Group by 'COD_SITO' and aggregate multiple values as lists
-aggregated_df = agg_merge.groupby('COD_SITO').agg({
+aggregated_df = sel_df.groupby('COD_SITO').agg({
     'Provincia':'first',
     'Comune':'first',
-    'Area':'first',
-    'Stato2017-2019': 'first',
+    'Stato': 'first',
+    'ANA_denom_sito': 'first',
     'ANA_classific_attuale': 'first',
     'descClassSuoli': 'first',
     'descClassAcque': 'first',
-    'Stato': 'first',
     'anno_ultimo_edma': 'first',
     'Tipologia_sito': 'first',
-    'ANA_denom_sito': 'first',
     'ANA_anno_apertura': 'first',
     'PRAT_anno_chiusura': 'first',
     
@@ -51,15 +57,14 @@ aggregated_df = agg_merge.groupby('COD_SITO').agg({
     'Matrice': lambda x: list(set(x.dropna())),  # Remove duplicates & NaN
     'tipo_sostanza': lambda x: list(set(x.dropna())),
     'sostanze': lambda x: list(set(x.dropna())),
-    'conc_max': lambda x: list(set(x.dropna())),
+    #'conc_max': lambda x: list(set(x.dropna())),
     'TecnologieDescrizione': lambda x: list(set(x.dropna())),
     'TipoTecnologiaDescrizione': lambda x: list(set(x.dropna())),
     'note_tecnologia': lambda x: list(set(x.dropna())),
     'MISE_descrizione': lambda x: list(set(x.dropna())),
     'MISP_descrizione': lambda x: list(set(x.dropna())),
-    'Simulato_SIAM2':'first'
 }).reset_index()
 
 # %%
-aggregated_df.to_excel(os.path.join(in_dir,"priori_merge_unique.xlsx"))
+aggregated_df.to_excel(os.path.join(in_dir,"tutti_merge_unique.xlsx"))
 # %%
