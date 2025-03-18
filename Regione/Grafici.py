4c607b9b3d56c93b57c77277b86e241df5c962f8
Claudia Medina <118851256+claumedimont@users.noreply.github.com>
2025-03-18 13:27:25 +0100

    graphs

--- Regione/Grafici.py
+++ Regione/Grafici.py
@@ -1,3 +1,12 @@
 '''
 GRAFICI
+
 '''
+
+# Load necessary packages and files
+import pandas as pd
+import os
+
+# Load the Excel files
+in_dir = 'C:/Users/user/OneDrive - Politecnico di Milano/04_REGIONE/SIAM2-AggiornamentoSitiMisure/Elaborazioni/' 
+main_df = pd.read_excel((os.path.join(in_dir,"tutti_merge_unique.xlsx")))
